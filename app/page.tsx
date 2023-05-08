490ca9c5d1d6788851bce0100b9409146c31dfc0
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2023-05-08 23:52:57 +0530

    Integrate login page with api

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,49 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      router.push("/dashboard");
+    }
+  }, []);
+
+  const handleLogin = () => {
+    if (email && password) {
+      setLoading(true);
+      const loginRequest = fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }).then((res) => res.json());
+      loginRequest
+        .then((res) => {
+          if (res?.token?.token) {
+            localStorage.setItem("access_token", res?.token?.token);
+            router.push("/dashboard");
+          } else {
+            alert("Invalid credentials");
+          }
+        })
+        .catch(() => {
+          alert("Invalid credentials");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  };
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -13,7 +57,7 @@ export default function Login() {
               className="space-y-4 md:space-y-6"
               onSubmit={(e) => {
                 e.preventDefault();
-                console.log(e);
+                handleLogin();
               }}
             >
               <div>
@@ -21,6 +65,8 @@ export default function Login() {
                   Your email
                 </label>
                 <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   type="email"
                   name="email"
                   id="email"
@@ -39,6 +85,8 @@ export default function Login() {
                   type="password"
                   name="password"
                   id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
@@ -46,9 +94,13 @@ export default function Login() {
 
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className={`w-full ${
+                  email && password && !loading
+                    ? "bg-slate-900"
+                    : "bg-slate-400 cursor-not-allowed"
+                } text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
               >
-                Sign in
+                {loading ? "Signing in..." : "Sign in"}
               </button>
             </form>
           </div>
