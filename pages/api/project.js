be13f0f77ebcab13d0964313c6278b1de6284aa5
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2023-05-09 19:55:20 +0530

    projects listing api added

--- pages/api/project.js
+++ pages/api/project.js
@@ -1,5 +1,10 @@
 import { getChunks, getContent, generateEmbeddings } from "../../utils";
+import { createClient } from "@supabase/supabase-js";
 
+const supabaseClient = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
+);
 const { v4: uuidv6 } = require("uuid");
 
 const handler = async (req, res) => {
@@ -8,22 +13,35 @@ const handler = async (req, res) => {
   const bodyParams = req.body;
   const request = { ...params, ...queryParams, ...bodyParams };
   const chunkedData = [];
-  const data = [];
+  let data = [];
   const project_id = uuidv6();
   const userEmail = "vguleria1108@gmail.com"; // todo get from token
-  if (!request?.projectName?.trim())
-    return res.status(400).json({ success: false, message: "Invalid request" });
-  const toFetch = request.urls?.length > 1000 ? 1000 : request.urls?.length;
-  for (let i = 0; i < toFetch; i++) {
-    const content = await getContent(request.urls?.[i]);
-    const chunkedContentData = await getChunks(content);
-    chunkedData.push(chunkedContentData);
-    data.push({ id: project_id, ...chunkedContentData });
+  if (req.method === "GET") {
+    const { data: projectsData, error } = await supabaseClient
+      .from("projects")
+      .select("project_name, project_id")
+      .eq("created_by", userEmail);
+    if (error) {
+      return res.status(400).json({ success: false, message: error.message });
+    }
+    return res.status(200).json({ success: true, data: projectsData });
+  } else {
+    if (!request?.projectName?.trim())
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid request" });
+    const toFetch = request.urls?.length > 1000 ? 1000 : request.urls?.length;
+    for (let i = 0; i < toFetch; i++) {
+      const content = await getContent(request.urls?.[i]);
+      const chunkedContentData = await getChunks(content);
+      chunkedData.push(chunkedContentData);
+      data.push({ id: project_id, ...chunkedContentData });
+    }
+    await generateEmbeddings(data, {
+      projectName: request?.projectName,
+      userEmail,
+    });
   }
-  await generateEmbeddings(data, {
-    projectName: request?.projectName,
-    userEmail,
-  });
   res.status(200).json({ success: true });
 };
 
