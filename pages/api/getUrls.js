bcad42bf83c0571c00ad96f3547233bb30b3e701
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2023-05-11 00:04:31 +0530

    Get urls from sitemap or other docgens like gitbook

---
+++ pages/api/getUrls.js
@@ -0,0 +1,44 @@
+import { load } from "cheerio";
+import axios from "axios";
+
+const handler = async (req, res) => {
+  const params = req.params;
+  const queryParams = req.query;
+  const bodyParams = req.body;
+  const request = { ...params, ...queryParams, ...bodyParams };
+  if (!request?.url)
+    return res.status(400).json({ success: false, message: "Invalid request" });
+  const url = request?.url;
+  let urlSet = new Set();
+  if (request?.type === "scrape") {
+    const html = await axios.get(url).catch((e) => {
+      console.log("html", url, e);
+    });
+    const $ = load(html.data);
+    $("nav a").each((i, el) => {
+      // console.log("el", el?.children?.[0]?.data);
+      const baseUrl = "https://" + url.split("/")[2];
+      if (el.attribs.href)
+        if (
+          el.attribs.href?.includes("http") &&
+          el.attribs.href?.includes(baseUrl)
+        ) {
+          urlSet.add(el.attribs.href);
+        } else if (!el.attribs.href?.includes("http")) {
+          urlSet.add(baseUrl + el.attribs.href);
+        }
+      console.log("content", urlSet);
+    });
+  } else {
+    await axios.get(url).then((response) => {
+      response.data.split("<loc>").forEach((url) => {
+        if (url.includes("https://")) {
+          urlSet.add(url.split("</loc>")[0]);
+        }
+      });
+    });
+  }
+  res.status(200).json({ success: false, data: Array.from(urlSet) });
+};
+
+export default handler;
