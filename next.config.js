173bbac01a6428a9642113bd37c2e128799cdd15
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2023-05-20 22:41:48 +0530

    something

--- next.config.js
+++ next.config.js
@@ -6,11 +6,12 @@ cron.schedule("* * * * *", async function () {
   const currentTime = new Date().getTime();
   if (currentTime - lastCron > 40000) {
     lastCron = currentTime;
-    fetch(process.env?.APP_URL + "/api/taskqueue")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("data", data);
-      });
+    if (process.env?.APP_URL)
+      fetch(process.env?.APP_URL + "/api/taskqueue")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data", data);
+        });
   }
 });
 
